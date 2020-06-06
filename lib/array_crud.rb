8dcf1a459372a5d598ba94c7f4878fa462cfa05a
PUNEET MANDIL <puneetmandil.mec18@itbhu.ac.in>
2020-06-06 08:57:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
 def create_an_empty_array
-  
+  arr = []
 end
 
 def create_an_array
-  
+  arr=[0,0,0,0]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array<< element
 end
 
 def add_element_to_start_of_array(array, element)
